dc9a25d28578dd761f96d905cda5c3d6ce84bca2
Tejas <tejaskhartude@gmail.com>
2021-11-07 15:30:56 +0530

    Added functionality of different types of maps at the toolbar menu

--- app/src/main/java/com/tejas/zooapp/MapsActivity.kt
+++ app/src/main/java/com/tejas/zooapp/MapsActivity.kt
@@ -2,6 +2,8 @@ package com.tejas.zooapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -30,15 +32,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_types_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.normal_map->{
+                map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+
+            R.id.hybrid_map->{
+                map.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+            }
+            R.id.satellite_map->{
+                map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+            R.id.terrain_map->{
+                map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+
+
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
@@ -52,7 +71,5 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }
 
-        //Setting Padding in Map Controls
-        map.setPadding(0,0,300,0)
     }
 }
